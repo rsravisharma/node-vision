7f359da5ac736e1f312c0becba3bf04c7bf3ec47
Ravi Sharma <rsravisharma38@gmail.com>
2024-01-03 17:30:52 +0530

    backend logic for aws image labels

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,60 @@
 var express = require('express');
 var router = express.Router();
+// Load the SDK
+var AWS = require('aws-sdk');
+const dotenv = require("dotenv");
+dotenv.config();
 
-router.post('/classify', function(req, res, next) {
+router.post('/classify', async function (req, res, next) {
   // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
-
+  let labelResponse = [];
+  
   // Your code starts here //
+
+  try {
+    const filePath = req.files;
+    console.log('the file is ', filePath);
+
+    AWS.config.update({
+      accessKeyId: process.env.AWS_ACCESS_KEY,
+      secretAccessKey: process.env.AWS_SECRET_KEY,
+      region: process.env.AWS_REGION,
+    });
+
+    const client = new AWS.Rekognition();
+    const params = {
+      Image: {
+        Bytes: filePath.file.data
+      },
+      MaxLabels: 10
+    };
+
+    const response = await client.detectLabels(params).promise();
+
+    console.log(`Detected labels for: ${filePath.file.name}`);
+    response.Labels.forEach((label) => {
+      labelResponse.push(label.Name);
+    });
+
+    console.log('the label names are ', labelResponse);
+
+    if (labelResponse.length < 1) {
+      labelResponse.push('No labels detected.');
+    }
+
+    res.json({
+      "labels": labelResponse
+    });
+
+  } catch (error) {
+    console.error('Error processing the request:', error);
+    res.status(500).json({
+      "error": "Unable to process the request"
+    });
+  }
 
   // Your code ends here //
 
-  res.json({
-    "labels": response
-  });
 });
 
 module.exports = router;
